1d858244903c73a186da47eb66855c5a5fb0e696
Sujith MP <147138494+Stoicsujith@users.noreply.github.com>
2025-07-29 08:39:27 +0530

    initial commit

---
+++ scripts/init_database.sql
@@ -0,0 +1,45 @@
+/*
+===================================================
+Creating database and schemas
+===================================================
+
+script purpose:
+     The script create a database called 'Datawarehouse' 
+     It checks if it already exists, if database exists it is dropped and recreated
+     And the script also creates three schemas Bronze, Silver and Gold
+
+Warning:
+    Running this script will remove the 'datawarehouse' database if already exists
+    Which leads to deleteion of data's which is already stored in the database
+    Make sure to check whether the database exists or not
+*/
+
+USE master;
+GO
+-- go is a command that signals the end of the batch of T-SQL statement
+
+--Drop and recreate database "datawarehouse" if exists
+IF EXISTS (SELECT 1 FROM  sys.databases WHERE name = 'DataWarehouse')
+Begin 
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DataWarehouse;
+END;
+GO
+
+--Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Creating Schemas
+CREATE SCHEMA bronze;
+GO
+
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
